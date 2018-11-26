489a57810176b9f536db4252e8193b316d7503ef
yamaura <ymruki@gmail.com>
2018-11-26 20:28:14 +0900

    Update alpine.sh

--- alpine.sh
+++ alpine.sh
@@ -1,1 +1,9 @@
-
+#!/bin/sh
+echo -p Disk-Path(/dev/sda):
+read disk
+if [ ! -d $disk ]; then
+  exit 1
+fi
+echo http://nl.alpinelinux.org/alpine/v3.8/main >> /etc/apk/repositories
+apk update
+apk add syslinux
