d68ea83a4cff97a27e423f790f500267a516c4db
yamaura <ymruki@gmail.com>
2018-11-26 20:37:17 +0900

    Update alpine.sh

--- alpine.sh
+++ alpine.sh
@@ -5,7 +5,23 @@ disk=/dev/sda
 grep -q ${disk} /proc/partitions || exit 1
 grep -q ${disk}1 /proc/partitions && exit 1
 
+cat | fdisk ${disk} <<EOF
+n
+p
+1
+
+
+a
+1
+t
+c
+w
+EOF
+
+modprobe vfat
+
 echo http://nl.alpinelinux.org/alpine/v3.8/main >> /etc/apk/repositories
 apk update
 apk add syslinux
 
+setup-bootable /media/cdrom ${disk}1
