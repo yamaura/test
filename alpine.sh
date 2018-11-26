46737d629fbc9b9d35d581fae02bf1bba07a9b28
yamaura <ymruki@gmail.com>
2018-11-26 22:32:33 +0900

    Update alpine.sh

--- alpine.sh
+++ alpine.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 export disk=/dev/sda
 
-grep -q ${disk} /proc/partitions || exit 1
-grep -q ${disk}1 /proc/partitions && exit 2
+grep -q ${disk##*/} /proc/partitions || exit 1
+grep -q ${disk##*/}1 /proc/partitions && exit 2
 
 cat | fdisk ${disk} <<EOF
 n
